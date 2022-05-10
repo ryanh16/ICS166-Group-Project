7ef51db59ed899dd1dcec9ac5646d0dde2b5c1c5
Boyuan Huang <ryanhuang0622@yeah.net>
2022-05-10 14:31:58 -0700

    Improved Wall Dialogue Behavior
    Improved wall dialogue behavior.

--- Assets/Scripts/Managers/InvisibleWallManager.cs
+++ Assets/Scripts/Managers/InvisibleWallManager.cs
@@ -9,11 +9,24 @@ public class InvisibleWallManager : MonoBehaviour
 
     [SerializeField]
     private QuittingGameFailManager QM;
+    private int RegularIndex = 0;
 
     private void OnTriggerEnter(Collider other)
     {
-        int randowIndex = Random.Range(0, DialoguePool.Length);
-        DialogueManager.SetDialogues(DialoguePool[randowIndex]);
+        // the wall will plays out all dialogue in order first
+        // then it will plays out dialogues randomly
+        int i = 0;
+        if (RegularIndex < DialoguePool.Length)
+        {
+            i = RegularIndex;
+            RegularIndex += 1;
+        }
+        else
+        {
+            i = Random.Range(0, DialoguePool.Length);
+        }
+
+        DialogueManager.SetDialogues(DialoguePool[i]);
         DialogueManager.StartDialogue();
         QM.enabled = true;
     }
