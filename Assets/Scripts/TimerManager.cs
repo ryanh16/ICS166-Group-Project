0445757fa6d3d306e678a4c25b42d2258c21015c
Marc Hagoriles <markyboyph@gmail.com>
2022-05-22 16:47:16 -0700

    Made a TimerManager Singleton
    Made the Timer a Singleton script

--- Assets/Scripts/TimerManager.cs
+++ Assets/Scripts/TimerManager.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Timer : MonoBehaviour
+public class TimerManager : MonoBehaviour
 {
+    #region Singleton
+    public static TimerManager Instance {get; private set;}
+    #endregion
+    
     #region Private Fields
     [SerializeField] private int numOfMinutes = 1;
-    [SerializeField] private Text timerText;
+    [SerializeField] private GameObject timerUI;
+    private Text timerText;
     private float currTime;
     private const int SECONDS_IN_MIN = 60;
     private string stringFormat = "{0:00}:{1:00}"; //This should format the Timer to 00:00.
@@ -15,13 +20,23 @@ public class Timer : MonoBehaviour
     #endregion
 
     #region Monobehaviour Callbacks
-    private void Start()
+    private void Awake()
     {
-        currTime = numOfMinutes * SECONDS_IN_MIN; //To start, currTime should hold the float value of minutes set in the Inspector.
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+        timerText = timerUI.GetComponentInChildren<Text>();
+
+        ResetTimer();
+        HideTimer();
     }
     private void Update()
     {
-        timerIsActive = true;
         if (timerIsActive)
         {
             currTime -= Time.deltaTime;
@@ -29,18 +44,32 @@ public class Timer : MonoBehaviour
             {
                 currTime = 0;
             }
+            displayTimer(currTime);
         }
-        displayTimer(currTime);
+       
+    }
+
+    private void OnDestroy()
+    {
+        Instance = null;
     }
     #endregion
 
     #region Helper Functions
     public void StartTimer()
     {
+        Debug.Log("Starting the Timer");
         timerIsActive = true;
     }
     public void StopTimer()
     {
+        Debug.Log("Stopping the Timer.");
+        timerIsActive = false;
+    }
+    public void ResetTimer()
+    {
+        Debug.Log("Resetting Timer");
+        currTime = numOfMinutes * SECONDS_IN_MIN;
         timerIsActive = false;
     }
     public void displayTimer(float time)
@@ -49,6 +78,11 @@ public class Timer : MonoBehaviour
         float seconds = Mathf.FloorToInt(time % SECONDS_IN_MIN);
 
         timerText.text = string.Format(stringFormat, minutes, seconds); //Format the timer to 00:00.
+    }
+    public void HideTimer()
+    {
+        Debug.Log("Hiding Timer UI.");
+        timerUI.SetActive(false);
     }
     #endregion
 }
